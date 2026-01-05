68567a054226501ec9f328d906ee424477b48dbc
hujaifa18 <abidhasanhujaifa1215@gmail.com>
2026-01-06 00:21:04 +0600

    Starting Java

---
+++ Hello/Test.java
@@ -0,0 +1,8 @@
+package Hello;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Today I am starting Java");
+    }
+}
